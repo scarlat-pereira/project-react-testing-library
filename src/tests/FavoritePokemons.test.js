83f7013241e2a12fe953bc25acfa8d689755072d
Scarlat Pereira <engcivil.scarlat@gmail.com>
2022-10-19 15:46:06 -0300

    cumprindo o requisito nº 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText(/no favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
